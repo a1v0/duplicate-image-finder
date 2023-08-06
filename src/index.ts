f7cfc2b4568367e3be4fa0123bc898b7c364bbe4
a1v0 <avc285@gmail.com>
2023-08-06 10:51:58 +0100

    Create plan of action

---
+++ src/index.ts
@@ -0,0 +1,10 @@
+const dotenv = require("dotenv").config();
+const fs = require("fs/promises");
+
+// get path from environment variable
+// gain access to contents of folder
+// access details of files e.g. size and type
+// create copy of two files but change name
+// see how you can compare the contents
+// check if HEIC files work in the same way
+// once all this is done, loop through all files
