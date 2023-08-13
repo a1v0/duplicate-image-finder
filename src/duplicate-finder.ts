6dedb3fb6b54d30b0c7205eddc05ef6979ef302f
a1v0 <avc285@gmail.com>
2023-08-13 19:25:30 +0100

    Add boilerplate to method

--- src/duplicate-finder.ts
+++ src/duplicate-finder.ts
@@ -1,7 +1,19 @@
-// get path from environment variable
 // gain access to contents of folder
 // access details of files e.g. size and type
 // create copy of two files but change name
 // see how you can compare the contents
 // check if HEIC files work in the same way
 // once all this is done, loop through all files
+
+import * as dotenv from "dotenv";
+dotenv.config();
+import * as fs from "fs/promises";
+
+const iCloudDir: string = process.env.ICLOUD_PATH ?? "";
+
+export const findDuplicates = () => {
+    if (!iCloudDir) {
+        console.error("ERROR: ENVIRONMENT VARIABLES FAILED TO LOAD");
+        return;
+    }
+};
