35105c8b67a76efc58ca1d3e2ef6ce9f665f6eba
a1v0 <avc285@gmail.com>
2023-08-13 15:49:04 +0100

    Create index logic to capture CLI args

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,19 @@
-// this file remains empty for now
-// I've put all logic into separate scripts
+const dotenv = require("dotenv").config();
+const fs = require("fs/promises");
+
+(() => {
+    // the first two args are spoken for. [2] is the received from CLI
+    const action = process.argv[2];
+
+    if (action === "comp") {
+        console.log("Running comparison between local and iCloud files.");
+        return;
+    }
+
+    if (action === "dupl") {
+        console.log("Identifying duplicate files in iCloud directory.");
+        return;
+    }
+
+    console.log("Please enter a valid script argument.");
+})();
