ebdacceb4e59ed0d74ac5249ec331edb1e2186d5
a1v0 <avc285@gmail.com>
2023-08-13 16:00:48 +0100

    Create action plan

---
+++ src/compare-local-with-icloud.ts
@@ -0,0 +1,12 @@
+const compareLocalWithICloud = () => {
+    // access both folders
+    // loop through each file in local folder
+    // find file of same name in iCloud
+    // compare size
+    // if size is different, add file name to an array
+    // at end, move all files in array to renaming folder
+    // before renaming those files, better to do a manual check to see if it's working the way it should
+    // before deleting files that aren't for renaming, best to check manually to see if it's working the way it should
+};
+
+module.exports = { compareLocalWithICloud };
