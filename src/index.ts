28feafda1fa12a6b79369c62c580372bb19799df
a1v0 <avc285@gmail.com>
2023-08-13 16:53:10 +0100

    Add line breaks to log statements

--- src/index.ts
+++ src/index.ts
@@ -5,15 +5,15 @@ import { compareLocalWithICloud } from "./compare-local-with-icloud";
     const action = process.argv[2];
 
     if (action === "comp") {
-        console.log("Running comparison between local and iCloud files.");
+        console.log("Running comparison between local and iCloud files.\n");
         compareLocalWithICloud();
         return;
     }
 
     if (action === "dupl") {
-        console.log("Identifying duplicate files in iCloud directory.");
+        console.log("Identifying duplicate files in iCloud directory.\n");
         return;
     }
 
-    console.log("Please enter a valid script argument.");
+    console.log("Please enter a valid script argument.\n");
 })();
