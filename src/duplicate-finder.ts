af9ab88ef62f6d11c71d98d12d4eb467015194ab
a1v0 <avc285@gmail.com>
2023-08-13 21:44:44 +0100

    Remove comments

--- src/duplicate-finder.ts
+++ src/duplicate-finder.ts
@@ -1,10 +1,3 @@
-// gain access to contents of folder
-// access details of files e.g. size and type
-// create copy of two files but change name
-// see how you can compare the contents
-// check if HEIC files work in the same way
-// once all this is done, loop through all files
-
 import * as dotenv from "dotenv";
 dotenv.config();
 import * as fs from "fs/promises";
