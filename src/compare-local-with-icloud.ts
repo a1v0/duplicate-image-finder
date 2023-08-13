3ebddb2485a7d73e8c61b1b01f3857139278603e
a1v0 <avc285@gmail.com>
2023-08-13 16:16:51 +0100

    Access contents of local dir

--- src/compare-local-with-icloud.ts
+++ src/compare-local-with-icloud.ts
@@ -1,6 +1,15 @@
-const compareLocalWithICloud = () => {
+import * as dotenv from "dotenv";
+dotenv.config();
+import * as fs from "fs/promises";
+
+export const compareLocalWithICloud = () => {
     // access both folders
+    const localDir: string = process.env.LOCAL_PATH ?? "";
+    fs.readdir(localDir).then((contents: Array<any>) => {
+        console.dir(contents);
+    });
     // loop through each file in local folder
+    // if file has no extension OR if extension is TXT, then ignore
     // find file of same name in iCloud
     // compare size
     // if size is different, add file name to an array
@@ -8,5 +17,3 @@ const compareLocalWithICloud = () => {
     // before renaming those files, better to do a manual check to see if it's working the way it should
     // before deleting files that aren't for renaming, best to check manually to see if it's working the way it should
 };
-
-module.exports = { compareLocalWithICloud };
